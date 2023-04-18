7381e1e06baeef7000516d74c719fdd9a1d6e340
oscarsaavedrax <103982163+oscarsaavedrax@users.noreply.github.com>
2023-04-18 14:58:20 -0700

    removed node_modules folder

--- src/contexts/socket.js
+++ src/contexts/socket.js
@@ -8,7 +8,10 @@ import React from "react";
 // Import socket.io for the client side - O.S.
 import { io } from "socket.io-client";
 // Set the url for the server
-const URL = "https://test-chat-server-491b.herokuapp.com/";
+const URL = "localhost:4000";
 // Create socket and autoconnect false to connect only when needed from other componenets - O.S.
-export const socket = io.connect(URL, { autoConnect: false });
+export const socket = io(URL, {
+  autoConnect: false,
+  transports: ["websocket"],
+});
 export const SocketContext = React.createContext();
